59bcdfbb477aeed789129b7105e6c4f6b9e110c9
ran7007 <korok7007@gmail.com>
2025-07-16 18:02:59 +0900

    메인페이지 수정

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     gsap.registerPlugin(ScrollTrigger);
+document.addEventListener("DOMContentLoaded", () => {
+    gsap.registerPlugin(ScrollTrigger);
 
-//     // 섹션 고정 스크롤 (스냅)
-//     let sections = gsap.utils.toArray(".snap-section");
-//     sections.forEach(section => {
-//         ScrollTrigger.create({
-//             trigger: section,
-//             start: "top top",
-//             pin: true,
-//             pinSpacing: false
-//         });
-//     });
+    // 섹션 고정 스크롤 (스냅)
+    let sections = gsap.utils.toArray(".snap-section");
+    sections.forEach(section => {
+        ScrollTrigger.create({
+            trigger: section,
+            start: "top top",
+            pin: true,
+            pinSpacing: false
+        });
+    });
 
-//     ScrollTrigger.create({
+    ScrollTrigger.create({
 
-//         snap: {
-//             snapTo: 1 / (sections.length - 1),
-//             duration: { min: 0.5, max: 1 },
-//             ease: "power3.inOut"
-//         }
-//     });
+        snap: {
+            snapTo: 1 / (sections.length - 1),
+            duration: { min: 0.5, max: 1 },
+            ease: "power3.inOut"
+        }
+    });
 
-//     // sec2 스크롤 효과
-//     gsap.fromTo("#sec2 .sec2_text > dl > *",
-//         { opacity: 0, y: 30 },
-//         {
-//             opacity: 1, y: 0,
-//             stagger: 0.2,
-//             ease: "power8.out",
-//             scrollTrigger: {
-//                 trigger: "#sec2",
-//                 start: "top 10%",
-//                 toggleActions: "play reverse play reverse"
-//             }
-//         }
-//     );
-// });
+    // sec2 스크롤 효과
+    gsap.fromTo("#sec2 .sec2_text > dl > *",
+        { opacity: 0, y: 30 },
+        {
+            opacity: 1, y: 0,
+            stagger: 0.2,
+            ease: "power8.out",
+            scrollTrigger: {
+                trigger: "#sec2",
+                start: "top 10%",
+                toggleActions: "play reverse play reverse"
+            }
+        }
+    );
+});
