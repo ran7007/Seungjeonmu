26bc1549666d5046ca475465c20f7b2a24fbbe50
ran7007 <korok7007@gmail.com>
2025-07-16 17:28:21 +0900

    메인페이지

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
-// $(document).ready(function () {
+// document.addEventListener("DOMContentLoaded", () => {
+//     gsap.registerPlugin(ScrollTrigger);
 
-//     $('.nav').hover(
-//         function () {
-//             // do nothing on enter
-//         },
-//         function () {
-//             // 마우스가 nav_wrap 영역 벗어나면 닫기
-//             $('.sub_gnb').stop(true, true).slideUp(200);
+//     // 섹션 고정 스크롤 (스냅)
+//     let sections = gsap.utils.toArray(".snap-section");
+//     sections.forEach(section => {
+//         ScrollTrigger.create({
+//             trigger: section,
+//             start: "top top",
+//             pin: true,
+//             pinSpacing: false
+//         });
+//     });
+
+//     ScrollTrigger.create({
+
+//         snap: {
+//             snapTo: 1 / (sections.length - 1),
+//             duration: { min: 0.5, max: 1 },
+//             ease: "power3.inOut"
+//         }
+//     });
+
+//     // sec2 스크롤 효과
+//     gsap.fromTo("#sec2 .sec2_text > dl > *",
+//         { opacity: 0, y: 30 },
+//         {
+//             opacity: 1, y: 0,
+//             stagger: 0.2,
+//             ease: "power8.out",
+//             scrollTrigger: {
+//                 trigger: "#sec2",
+//                 start: "top 10%",
+//                 toggleActions: "play reverse play reverse"
+//             }
 //         }
 //     );
-
-//     $('.gnb li').hover(function () {
-//         var index = $(this).data('index');
-
-//         $('.sub_gnb').stop(true, true).slideDown(200);
-//         $('.sub_gnb .depth2').hide().eq(index).show();
-//     });
 // });
