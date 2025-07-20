51ecdc3fda4ab889174094acd906c77496011715
ran7007 <korok7007@gmail.com>
2025-07-21 01:08:52 +0900

    주요활동 다시

--- js/Activities.js
+++ js/Activities.js
@@ -45,3 +45,11 @@ window.addEventListener('scroll', () => {
         tabImg.style.opacity = '0';
     }
 });
+
+function openTab(tabName) {
+    document.querySelectorAll('.tab, .tabs').forEach(function (el) {
+        el.classList.remove('on');
+    });
+    document.getElementById(tabName).classList.add('on');
+    document.querySelector('.tabs[onclick="openTab(\'' + tabName + '\')"]').classList.add('on');
+}
