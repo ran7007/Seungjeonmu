7d5e6ac7f784931c354272f44e46e89679b66e9a
ran7007 <korok7007@gmail.com>
2025-07-22 18:00:15 +0900

    오시는 길 수정

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+//스크롤
+$(document).ready(function () {
+    $(".gnb li a[href^='#']").click(function (e) {
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500);
+    });
+});
+
 
 $(function () {
     scrollAnimations();
@@ -42,22 +52,9 @@ $(function () {
         }
     });
 
-
-    //**************오버레이 나중에 문제 해결 하기***************** */
-    // gsap.to("#sec2 .overlay", {
-    //     opacity: 0.1,
-    //     ease: "none",
-    //     scrollTrigger: {
-    //         trigger: "#sec2",
-    //         start: "top bottom",
-    //         end: "top top",
-    //         scrub: true
-    //     }
-    // });
-
     ScrollTrigger.create({
         trigger: "#sec3",
-        start: "top 80%",
+        start: "bottom 80%",
         onEnter: () => {
             gsap.to("#sec1 .sec1_img", { opacity: 0, y: -50, duration: 1, ease: "power2.out" });
         },
@@ -66,3 +63,44 @@ $(function () {
         },
     });
 }
+
+// 패널 리스트 확보 (.sec2 ~ .sec4 모두)
+let panels = gsap.utils.toArray(".sec2, .sec3, .sec4");
+
+panels.forEach((panel) => {
+    ScrollTrigger.create({
+        trigger: panel,
+        start: "top top",
+        end: "bottom top",
+        pin: true,
+        pinSpacing: true,
+    });
+});
+
+ScrollTrigger.create({
+    snap: {
+        snapTo: (progress, self) => {
+            const scrollPos = self.scroll();
+            const closest = gsap.utils.snap(snapScrollPositions, scrollPos);
+            return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), closest);
+        },
+        duration: 1,
+        ease: "power1.inOut"
+    }
+});
+
+gsap.utils.toArray(".sec3_img, .sec4_img").forEach((img) => {
+    gsap.fromTo(img,
+        { scale: 1 },
+        {
+            scale: 1.3,
+            scrollTrigger: {
+                trigger: img,
+                start: "center bottom",
+                end: "bottom top",
+                scrub: true,
+                ease: "none"
+            }
+        }
+    );
+});
