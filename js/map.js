fda98c1d9afcdb8c528486f8e0019a7d51f7d4f3
ran7007 <korok7007@gmail.com>
2025-07-23 11:58:00 +0900

    오시는 길 반응형 완

---
+++ js/map.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const ham = document.querySelector(".ham");
+    const mgnb = document.querySelector(".mgnb");
+
+    ham.addEventListener("click", () => {
+        ham.classList.toggle("active");
+        mgnb.classList.toggle("active");
+    });
+});
